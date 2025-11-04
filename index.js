9879634f9c5ad5982051548a397535fa5b157c16
gintingprojects19 <162718453+gintingprojects19@users.noreply.github.com>
2025-11-04 12:43:22 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var nol = document.getElementById("nol");
+var pilih =document.getElementById("pilih");
+var lanjut = document.getElementById("lanjut");
+
+lanjut.addEventListener("click",function(){
+    if(pilih.value=="nol"){
+        alert("anda belum memilih kelas")
+    
+    }
+    if(pilih.value=="binggrislanjut12"){
+        /*window.open("https://www.gmail.com");*/
+    }
+    if(pilih.value=="binggrislanjut11"){
+        window.open("https://gintingprojects19.github.io/gintinglms/?i=1");
+    }
+    if(pilih.value=="binggris12"){
+        /*window.open("https://www.gmail.com");*/
+    }
+     if(pilih.value=="binggris11"){
+        /*window.open("https://www.gmail.com");*/
+    }
+    if(pilih.value=="binggris10"){
+        /*window.open("https://www.gmail.com");*/
+    }
+})
